1fb797cc70ad507918e26b5a0b0432fc58876ca9
Larson Collier <collierlarson@gmail.com>
2022-10-23 23:28:57 -0400

    commit tree 6edcfe06046087ea767a5b46073cf6751f31ecb9

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const leave = document.getElementById('main')
+leave.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Larson is the champion";
